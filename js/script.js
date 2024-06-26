d2821e99b47d31df5fb957a9afb53f0780dffa02
mariannacoppa <densimetra@gmail.com>
2024-06-26 17:15:53 +0200

    [UPD] text centered

--- js/script.js
+++ js/script.js
@@ -3,27 +3,23 @@ const btn = document.getElementById("calculate-price");
 btn.addEventListener('click', function () {
     let firstAndLastName = document.getElementById('FirstAndLastName').value;
     let km = document.getElementById('km').value;
-    let eta = document.getElementById('eta').id;
-    console.log(firstAndLastName, eta, prezzo_finale);
+    let eta = document.getElementById('eta').value;
+    console.log(firstAndLastName, km, eta, prezzo_finale);
 });
-// let km = prompt("Insrisci i km da percorrere nel tuo viaggio");
-// console.log("km");
-// let eta = prompt("Inserisci la tua età");
-// console.log("età");
 // Creo variabile a cui assegno come valore il prodotto tra il valore km ed il valore età
 let prezzo_base = km * 0.21;
 let prezzo_finale = prezzo_base;
 // programmo gli sconti
-if (eta = minorenne) {
+if (eta < 18) {
     let sconto = prezzo_base * 0.2;
     prezzo_finale = prezzo_base - sconto;
     // prezzo_finale = prezzo_finale * 0.8;
 }
-else if (eta = senior) {
+else if (eta > 65) {
     let sconto = prezzo_base * 0.4;
     prezzo_finale = prezzo_base - sconto;
     // prezzo_finale = prezzo_base * 0.6;
-}
+};
 console.log(prezzo_finale);
 // arrotondo il valore alla seconda cifra decimale
 let finale = prezzo_finale.toFixed(2);
