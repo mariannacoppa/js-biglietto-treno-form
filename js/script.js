b0e3dc49e4a6cc7881ffb84360ea7bff8e5b209f
mariannacoppa <densimetra@gmail.com>
2024-06-26 17:27:13 +0200

    [FIX] insert all code rows in click function

--- js/script.js
+++ js/script.js
@@ -4,22 +4,22 @@ btn.addEventListener('click', function () {
     let firstAndLastName = document.getElementById('FirstAndLastName').value;
     let km = document.getElementById('km').value;
     let eta = document.getElementById('eta').value;
+    let prezzo_base = km * 0.21;
     console.log(firstAndLastName, km, eta, prezzo_finale);
+    // Creo variabile a cui assegno come valore il prodotto tra il valore km ed il valore età
+    let prezzo_finale = prezzo_base;
+    // programmo gli sconti
+    if (eta < 18) {
+        let sconto = prezzo_base * 0.2;
+        prezzo_finale = prezzo_base - sconto;
+        // prezzo_finale = prezzo_finale * 0.8;
+    }
+    else if (eta > 65) {
+        let sconto = prezzo_base * 0.4;
+        prezzo_finale = prezzo_base - sconto;
+        // prezzo_finale = prezzo_base * 0.6;
+    };
 });
-// Creo variabile a cui assegno come valore il prodotto tra il valore km ed il valore età
-let prezzo_base = km * 0.21;
-let prezzo_finale = prezzo_base;
-// programmo gli sconti
-if (eta < 18) {
-    let sconto = prezzo_base * 0.2;
-    prezzo_finale = prezzo_base - sconto;
-    // prezzo_finale = prezzo_finale * 0.8;
-}
-else if (eta > 65) {
-    let sconto = prezzo_base * 0.4;
-    prezzo_finale = prezzo_base - sconto;
-    // prezzo_finale = prezzo_base * 0.6;
-};
 console.log(prezzo_finale);
 // arrotondo il valore alla seconda cifra decimale
 let finale = prezzo_finale.toFixed(2);
