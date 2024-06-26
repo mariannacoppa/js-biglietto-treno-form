28952ba2cd97ce063a1677918de2078acc3c1e85
mariannacoppa <densimetra@gmail.com>
2024-06-26 16:25:44 +0200

    [UPD] link button for price calculation

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
-// Permetto l'inserimento da parte dell'utente del nome e cognome
-let firstAndLastName = document.getElementById("exampleInputFirstAndLastName1");
-console.log(firstAndLastName);
-// Permetto l'inserimento da parte dell'utente del numero di km da percorrere
+// Recupero il pulsante per il calcolo del prezzo
+const btn = document.getElementById("calculate-price");
+btn.addEventListener('click', function () {
+    let firstAndLastName = document.getElementById('FirstAndLastName').value;
+    let km = document.getElementById('km').value;
+    let eta = document.getElementById('eta').id;
+    console.log(firstAndLastName, eta, prezzo_finale)
+});
 // let km = prompt("Insrisci i km da percorrere nel tuo viaggio");
 // console.log("km");
-// Permetto l'inserimento da parte dell'utente dell'età del passeggero
 // let eta = prompt("Inserisci la tua età");
 // console.log("età");
 // Creo variabile a cui assegno come valore il prodotto tra il valore km ed il valore età
 let prezzo_base = km * 0.21;
 let prezzo_finale = prezzo_base;
 // programmo gli sconti
-if (eta < 18) {
+if (eta = minorenne) {
     let sconto = prezzo_base * 0.2;
     prezzo_finale = prezzo_base - sconto;
     // prezzo_finale = prezzo_finale * 0.8;
 }
-else if (eta > 64) {
+else if (eta = senior) {
     let sconto = prezzo_base * 0.4;
     prezzo_finale = prezzo_base - sconto;
     // prezzo_finale = prezzo_base * 0.6;
