c3e5f94918d168db01f1c498120ce20973dbf431
mariannacoppa <densimetra@gmail.com>
2024-06-27 10:14:50 +0200

    [FIX] change form tag in div tag and changed age input in select input

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Recupero il pulsante per il calcolo del prezzo
-const btn = document.getElementById("calculate-price");
+const btn = document.getElementById("calculate");
 btn.addEventListener('click', function () {
     let firstAndLastName = document.getElementById('firstAndLastName').value;
     let km = document.getElementById('km').value;
@@ -7,15 +7,21 @@ btn.addEventListener('click', function () {
     let prezzo_base = km * 0.21;
     let prezzo_finale = prezzo_base;
     // programmo gli sconti
-    if (eta < 18) {
+    if (eta == 1) {
         let sconto = prezzo_base * 0.2;
         prezzo_finale = prezzo_base - sconto; 
         // prezzo_finale = prezzo_finale * 0.8;
     }
-    else if (eta > 64) {
+    else if (eta == 0) {
+        prezzo_finale = prezzo_base;
+    }
+    else if (eta == 2) {
         let sconto = prezzo_base * 0.4;
         prezzo_finale = prezzo_base - sconto;
         // prezzo_finale = prezzo_base * 0.6;
+    }
+    else {
+        alert("Non hai selezionato l'età: verrà applicato il prezzo base.");
     }
     console.log(prezzo_finale);
     // arrotondo il valore alla seconda cifra decimale
